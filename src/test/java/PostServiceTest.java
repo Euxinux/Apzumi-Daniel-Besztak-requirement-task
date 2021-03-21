441877855dcd03faa1dbb9125fc90e9312c2624a
Dijkstra <besztak.daniel@gmail.com>
2021-03-21 21:35:31 +0100

    Commit 2.4 * Correct named method in PostServiceTest.java; Daniel Besztak

--- src/test/java/PostServiceTest.java
+++ src/test/java/PostServiceTest.java
@@ -1,51 +1,44 @@
-
 import org.junit.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.TestPropertySource;
 
 import static io.restassured.RestAssured.given;
-
-
-
 public class PostServiceTest {
 
     @Test
-    public void displayAllPostStatusCode200() {
+    public void displayAllPost_OK_StatusCode200() {
         given().when().get("/posts").then().statusCode(200);
     }
 
     @Test
-    public void displayPostWithoutUserIdStatusCode200() {
+    public void displayPostWithoutUserId_OK_StatusCode200() {
         given().when().get("/posts/withoutId").then().statusCode(200);
     }
 
     @Test
-    public void postTitleIsntExistInDBStatusCode204() {
+    public void editPostTitle_PostIsntExist_StatusCode204() {
         given().pathParam("postTitle", "New Title").when().get("posts/{postTitle}").then().statusCode(204);
     }
 
     @Test
-    public void wrongAuthorPostStatusCode401() {
+    public void editPostBody_WrongAuthorPost_StatusCode401() {
         given(given().body(" ").pathParam("id", 1).header("userid", 10)).when().put("/posts/body/{id}").then().statusCode(401);
     }
 
     @Test
-    public void correctlyChangePostTitleStatusCode200() {
+    public void editPostBody_CorrectlyAuthor_StatusCode200() {
         given(given().body(" ").pathParam("id", 1).header("userid", 1)).when().put("/posts/title/{id}").then().statusCode(200);
     }
 
     @Test
-    public void correctlyUpdateDBByUserStatusCode200(){
+    public void updateDB_CorrectUpdate_StatusCode200(){
         given().when().put("/rest").then().statusCode(200);
     }
 
     @Test
-    public void WrongAuthorPostToDeleteStatusCode401(){
+    public void deletePost_WrongAuthorPost_StatusCode401(){
         given().pathParam("id",1).header("userid",10).when().delete("/posts/{id}").then().statusCode(401);
     }
     @Test
-    public void PostToDeleteDoesntExistStatusCode204(){
+    public void deletePost_PostDoesntExist_StatusCode204(){
         given().pathParam("id",111).header("userid",10).when().delete("/posts/{id}").then().statusCode(204);
     }
 
